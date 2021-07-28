ebc26f758a6905357dd339d5d17b5a3efd045760
Yogita Kumar <61360028+yogitakumar@users.noreply.github.com>
2021-07-28 17:27:11 +0100

    Add files via upload

---
+++ LockDemo/LockDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace LockDemo
+{
+    internal class Program
+    {
+        private static readonly object _object = new object();
+
+        private static void TestLock()
+        {
+            lock (_object)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine(Environment.TickCount);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                ThreadStart start = new ThreadStart(TestLock);
+                new Thread(start).Start();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
